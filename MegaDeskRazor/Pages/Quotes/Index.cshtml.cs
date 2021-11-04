b2e643b73f7d8d81830e8d9f12ee9d0861bd0558
Tanner Stratford <tannerstratford1@gmail.com>
2021-11-04 12:44:22 -0600

    Added filtering

--- MegaDeskRazor/Pages/Quotes/Index.cshtml.cs
+++ MegaDeskRazor/Pages/Quotes/Index.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using MegaDeskRazor.Data;
 using MegaDeskRazor.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace MegaDeskRazor.Pages.Quotes
 {
@@ -19,11 +20,29 @@ namespace MegaDeskRazor.Pages.Quotes
             _context = context;
         }
 
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        public SelectList SurfaceMaterials { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string DeskSurfaceMaterial { get; set; }
+
         public IList<DeskQuote> DeskQuote { get;set; }
 
         public async Task OnGetAsync()
         {
-            DeskQuote = await _context.DeskQuote
+            IQueryable<string> surfaceMaterialQuery = from m in _context.Desk
+                                            orderby m.SurfaceMaterial
+                                            select m.SurfaceMaterial;
+
+            var desks = from d in _context.DeskQuote
+                         select d;
+
+            if (!string.IsNullOrEmpty(DeskSurfaceMaterial))
+            {
+                desks = desks.Where(x => x.Desk.SurfaceMaterial == DeskSurfaceMaterial);
+            }
+            SurfaceMaterials = new SelectList(await surfaceMaterialQuery.Distinct().ToListAsync());
+            DeskQuote = await desks
                 .Include(d => d.Desk).ToListAsync();
         }
     }
